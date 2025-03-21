10efcd86a697bd21d300f5e477c0f73c85574c84
Kedargajmal <Kedargajmal8@gmail.com>
2025-03-21 22:07:40 +0530

    Add files via upload

---
+++ drizzle.config.js
@@ -0,0 +1,10 @@
+export default {
+  dialect: "postgresql",
+  schema: "./src/utils/schema.jsx",
+  out: "./drizzle",
+  dbCredentials: {
+    url: "postgresql://smart_cancer_owner:FpvZ7bajEML9@ep-cold-flower-a5c3320i-pooler.us-east-2.aws.neon.tech/smart_cancer?sslmode=require",
+    connectionString:
+      "postgresql://smart_cancer_owner:FpvZ7bajEML9@ep-cold-flower-a5c3320i-pooler.us-east-2.aws.neon.tech/smart_cancer?sslmode=require",
+  },
+};
